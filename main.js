adc06b81e0f9adb97e237208d9f2d41d0a5935d4
niels-linden <60019402+niels-linden@users.noreply.github.com>
2022-02-04 15:34:46 +0100

    Complete challenge

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      selectedVariant: 0,
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+          onSale: true,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+          onSale: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    onSale() {
+      if (this.variants[this.selectedVariant].onSale) {
+        return this.brand + " " + this.product + " is on sale";
+      }
+    },
+  },
+});
