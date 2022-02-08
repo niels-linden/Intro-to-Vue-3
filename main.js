85f5b41fa368806e16e5b31f925968fd690473c8
niels-linden <60019402+niels-linden@users.noreply.github.com>
2022-02-08 11:11:11 +0100

    Complete challenge

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    removeById(id) {
+      let index = this.cart.indexOf(id);
+      this.cart.splice(index, 1);
+    },
+  },
+});
